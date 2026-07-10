446c585c733343fa07721a040267330a0ec07e4f
investormachuki-bit <investormachuki@gmail.com>
2026-07-10 10:52:13 +0300

    Update AppShell.tsx

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -7,13 +7,25 @@ type AppShellProps = {
 
 export default function AppShell({ children }: AppShellProps) {
   return (
-    <div className="flex min-h-screen bg-slate-100">
-      <Sidebar />
+    <div className="min-h-screen bg-slate-100">
+      {/* Desktop */}
+      <div className="hidden md:flex">
+        <Sidebar />
 
-      <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col">
+          <Topbar />
+
+          <main className="flex-1 p-6">
+            {children}
+          </main>
+        </div>
+      </div>
+
+      {/* Mobile */}
+      <div className="md:hidden">
         <Topbar />
 
-        <main className="flex-1 p-6">
+        <main className="p-4">
           {children}
         </main>
       </div>
