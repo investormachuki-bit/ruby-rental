b2199dbe216558c0821ad23182a0d3832b9445f1
investormachuki-bit <investormachuki@gmail.com>
2026-07-10 09:40:59 +0300

    Create AppShell.tsx

---
+++ components/layout/AppShell.tsx
@@ -0,0 +1,11 @@
+type AppShellProps = {
+  children: React.ReactNode;
+};
+
+export default function AppShell({ children }: AppShellProps) {
+  return (
+    <div className="min-h-screen bg-slate-100">
+      {children}
+    </div>
+  );
+}
