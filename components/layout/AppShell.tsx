242ef258456ed564f972529d16263795fd0b36d1
investormachuki-bit <investormachuki@gmail.com>
2026-07-10 10:09:01 +0300

    Update AppShell.tsx

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -1,11 +1,22 @@
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+
 type AppShellProps = {
   children: React.ReactNode;
 };
 
 export default function AppShell({ children }: AppShellProps) {
   return (
-    <div className="min-h-screen bg-slate-100">
-      {children}
+    <div className="flex min-h-screen bg-slate-100">
+      <Sidebar />
+
+      <div className="flex flex-1 flex-col">
+        <Topbar />
+
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
