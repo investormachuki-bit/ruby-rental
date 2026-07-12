2100303ad9b7b06d31350404aec5eac7ea3b0e61
investormachuki-bit <investormachuki@gmail.com>
2026-07-12 23:41:43 +0300

    Create page.tsx

---
+++ app/units/page.tsx
@@ -0,0 +1,10 @@
+import AppShell from "@/components/layout/AppShell";
+import UnitsPage from "@/components/units/UnitsPage";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <UnitsPage />
+    </AppShell>
+  );
+}
