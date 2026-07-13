2c106930c3148efbac4ed7d518c4c455721b5121
investormachuki-bit <investormachuki@gmail.com>
2026-07-13 16:16:26 +0300

    Create page.tsx

---
+++ app/leases/page.tsx
@@ -0,0 +1,10 @@
+import AppShell from "@/components/layout/AppShell";
+import LeasesPage from "@/components/leases/LeasesPage";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <LeasesPage />
+    </AppShell>
+  );
+}
