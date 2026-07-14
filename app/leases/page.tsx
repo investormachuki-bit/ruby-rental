df5f9198b34a596cde277c9bd7c815eb81259217
investormachuki-bit <investormachuki@gmail.com>
2026-07-14 22:41:12 +0300

    Update page.tsx

--- app/leases/page.tsx
+++ app/leases/page.tsx
@@ -1,10 +1,5 @@
-import AppShell from "@/components/layout/AppShell";
 import LeasesPage from "@/components/leases/LeasesPage";
 
 export default function Page() {
-  return (
-    <AppShell>
-      <LeasesPage />
-    </AppShell>
-  );
+  return <LeasesPage />;
 }
