f32d59f46b150fd25824a88992de1c9e0ee736fe
investormachuki-bit <investormachuki@gmail.com>
2026-07-14 21:03:02 +0300

    Update page.tsx

--- app/units/page.tsx
+++ app/units/page.tsx
@@ -1,10 +1,5 @@
-import AppShell from "@/components/layout/AppShell";
 import UnitsPage from "@/components/units/UnitsPage";
 
 export default function Page() {
-  return (
-    <AppShell>
-      <UnitsPage />
-    </AppShell>
-  );
+  return <UnitsPage />;
 }
