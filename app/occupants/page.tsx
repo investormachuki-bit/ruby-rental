1f4166b26506707fe79675f1e6f53300f02d6629
investormachuki-bit <investormachuki@gmail.com>
2026-07-14 21:20:42 +0300

    Update page.tsx

--- app/occupants/page.tsx
+++ app/occupants/page.tsx
@@ -1,10 +1,5 @@
-import AppShell from "@/components/layout/AppShell";
 import OccupantsPage from "@/components/occupants/OccupantsPage";
 
 export default function Page() {
-  return (
-    <AppShell>
-      <OccupantsPage />
-    </AppShell>
-  );
+  return <OccupantsPage />;
 }
