f9aae83d89bb0fc25e852bccdbbff8e99b9a3d94
investormachuki-bit <investormachuki@gmail.com>
2026-07-15 15:22:16 +0300

    Update AppShell.tsx

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useState } from "react";
-
-import Sidebar from "./Sidebar";
+import Sidebar from "./sidebar/Sidebar";
 import Topbar from "./Topbar";
 
 type AppShellProps = {
