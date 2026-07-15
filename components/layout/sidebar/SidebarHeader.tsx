925ac4128f7e5795b471814c603eb28b50470645
investormachuki-bit <investormachuki@gmail.com>
2026-07-15 15:16:59 +0300

    Create SidebarHeader.tsx

---
+++ components/layout/sidebar/SidebarHeader.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Logo from "../Logo";
+
+export default function SidebarHeader() {
+
+  return (
+
+    <div className="border-b border-[#232323] p-6">
+
+      <Logo />
+
+    </div>
+
+  );
+
+}
