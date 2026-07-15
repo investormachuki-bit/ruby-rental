8bdfcdb95e72984602466e43d77a76a49d05cf01
investormachuki-bit <investormachuki@gmail.com>
2026-07-15 15:18:19 +0300

    Create SidebarFooter.tsx

---
+++ components/layout/sidebar/SidebarFooter.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function SidebarFooter() {
+
+  return (
+
+    <div className="border-t border-[#232323] p-5">
+
+      <p className="text-center text-xs text-gray-600">
+
+        Ruby Rental v1.0
+
+      </p>
+
+    </div>
+
+  );
+
+}
