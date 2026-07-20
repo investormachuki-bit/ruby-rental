137fcce0e4f1dd6ce6a1350ac883555f1ee3eedf
investormachuki-bit <investormachuki@gmail.com>
2026-07-20 14:10:51 +0300

    Create page.tsx

---
+++ app/tenants/[id]/page.tsx
@@ -0,0 +1,19 @@
+import TenantProfilePage from "@/components/tenants/TenantProfilePage";
+
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function Page({
+  params,
+}: Props) {
+  const { id } = await params;
+
+  return (
+    <TenantProfilePage
+      tenantId={id}
+    />
+  );
+}
